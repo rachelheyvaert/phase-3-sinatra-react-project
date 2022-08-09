a3fee183fc7cdad4e1be1bf483a3b17a1f95a96b
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-08-09 07:20:35 -0400

    Get and Post requests in Todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,16 @@
 class TodosController < ApplicationController
+get "/todos" do
+    todos = Todo.all
+    todos.to_json
+end 
 
+
+post "/todos" do
+todo = Todo.new(params[:todo])
+if todo.save
+    todo.to_json
+else
+    {erros: todo.errors.full_message }.to_json
+end
+end
 end
