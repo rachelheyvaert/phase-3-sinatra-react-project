7b25350b70ea2913a8118210e0c9c91a81d1320e
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-08-16 08:27:55 -0400

    Created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,16 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+category_1 = Category.create(title: "Daily")
+category_2 = Category.create(title: "Weekly")
+category_3 = Category.create(title: "Monthly")
+category_4 = Category.create(title: "Yearly")
+
+
+Todo.create(name: "Make bed", details: "Pick pillows off floor and place on bed", category_id: category_1)
+Todo.create(name: "Do Laundry", details: "Wash clothes and sheets", category_id: category_2)
+Todo.create(name: "Make a budget", details: "Review spending and forcast for next month", category_id: category_3)
+Todo.create(name: "Oil Change", details: "Take car to shop", category_id: category_4)
+
 
 puts "✅ Done seeding!"
