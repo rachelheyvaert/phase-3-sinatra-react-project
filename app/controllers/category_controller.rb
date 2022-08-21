c5746c55a3c684b3bedf6f846b0473c5b0e0b75c
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-08-21 14:42:32 -0400

    CRUD in controllers

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,3 +1,7 @@
 class CategoriesController < ApplicationController
+get '/categories' do
+    @categories = Category.all
+    @categories.to_json(include: [:todos])
+end
 
 end
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -13,4 +13,12 @@ else
     {erros: todo.errors.full_message }.to_json
 end
 end
+
+patch "/todos/:id" do
+    # todo = Todo.find(params[:id])
+    # if todo && todo.update(params[:todo])
+    #     params#iterate over table aspects?
+    #     updated_todo = todo.create
+    # end
+end
 end
