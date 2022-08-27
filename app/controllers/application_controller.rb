ea0fa8349f1533be9b48641840dfda863a714df0
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-08-27 13:23:59 -0400

    Create and read Crud installed in categ controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  #split them up
-  # get "/" do
-   #home
-# end 
-
 
 end
--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -5,7 +5,7 @@ get '/categories' do
     @categories.to_json(include: [:todos])
 end
 
-def show
-    render json: @category, include: [:todos]
+post '/categories' do
+    format_as_json(Category.create(category_params))
 end
 end
