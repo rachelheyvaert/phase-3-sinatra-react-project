10c6742af3ad2b1d757d3d23b564bce3426248e6
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-09-14 18:08:45 -0400

    Updated params in todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,15 @@
 require 'pry'
 class TodosController < ApplicationController
-# GET
+
     get "/todos" do
         todos = Todo.order(:name)
         todos.to_json(include: :category)
     end 
+
+    get "/todos/:id" do
+        todo = Todo.find_by(id: params[:id])
+        todo.to_json
+    end
 
     post "/todos" do
         category = Category.find_by(id: params[:category_id])
@@ -13,16 +18,23 @@ class TodosController < ApplicationController
     end
 
     patch "/todos/:id" do
-        category = Category.find_by(id: params[:category_id])
-        todo = category.todos.find_by(id: params[:id])
+        # binding.pry
+        # category = Category.find_by(id: params[:todo][:category_id])
+        todo = Todo.find_by(id: params[:id].to_i)
+        # binding.pry
         todo.update(
             name: params[:name],
             details: params[:details],
-            completed: params[false],
+            completed: false,
+            category_id: params[:category_id].to_i
         )
-        todo.category = category
+        # todo.category = category
+        # category.update(
+        #     title: params[:title]
+        # )
         todo.save
         todo.to_json
+   
     end
 
     delete "/todos/:id" do
